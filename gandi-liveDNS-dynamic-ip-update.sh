8861e8fc7ddee7c00fc7c00feb79fe5873e4efb5
ArthurAtangana <arthur.atangana@protonmail.com>
2023-03-23 23:39:56 -0400

    init bash script

---
+++ gandi-liveDNS-dynamic-ip-update.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Customize with your data
+APIKEY="input-api"
+RECORD="@"
+echo ${RECORD}
+# DO NOT EDIT AFTER THIS LINE
+CURRENTIP=$(curl -s ifconfig.co/ip)
+IPLENGTH=$(echo -n ${CURRENTIP} | wc -m)
+
+# Check that ifconfig.co returns an IP
+if [ ${IPLENGTH} -gt 30 ]
+then
+  exit 1
+fi
+
+# Get gandi's NS for my domain
+NS=$(curl -s --header "Authorization: Apikey ${APIKEY}" https://api.gandi.net/v5/livedns/domains/${DOMAIN}/nameservers | jq  --raw-output '.[0] | sub("^.*?_"; "")')
+#echo "NameServer: $NS"
+# Get the last IP recorded
+LASTREGISTEREDIP=$(dig +short ${RECORD}.${DOMAIN} @${NS})
+
+# Update if needed
+if [ "${CURRENTIP}" != "${LASTREGISTEREDIP}" ]
+then
+  curl -X PUT \
+    https://api.gandi.net/v5/livedns/domains/${DOMAIN}/records/${RECORD}/A \
+    -H "authorization: Apikey ${APIKEY}" \
+    -H 'content-type: application/json' \
+    -d "{\"rrset_values\": [\"${CURRENTIP}\"], \"rrset_ttl\": "320"}"
+else
+  echo "IP up to date"
+  echo "last registered ip:$LASTREGISTEREDIP"
+  echo "current ip:$CURRENTIP"
+fi
+echo "Done"
